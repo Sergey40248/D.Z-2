5c5d1bbb0a26dcf8b4b076bb1d355cfd1cea3479
sergey <Sergey40248@mail.ru>
2022-09-27 21:30:26 +0300

    удаляет вторую цифру трехзначного случайного числа.

---
+++ zadanie1/Program.cs
@@ -0,0 +1,8 @@
+﻿/*11. Напишите программу,
+которая выводит случайное трёхзначное число
+и удаляет вторую цифру этого числа.*/
+
+int number = new Random ().Next(100,1000);
+Console.WriteLine(number);
+
+Console.Write($"{number/100}{number%10}");
